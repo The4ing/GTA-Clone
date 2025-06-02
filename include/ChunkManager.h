7fce6411075e260d5787e89847bcae315168380a
The4ing <atar.orram101@gmail.com>
2025-06-02 23:32:47 +0300

    Update ChunkManager.h

--- include/ChunkManager.h
+++ include/ChunkManager.h
@@ -8,7 +8,6 @@
 
 struct IntPairHash {
     std::size_t operator()(const std::pair<int, int>& p) const noexcept {
-
         std::uint64_t key = (static_cast<std::uint64_t>(p.first) << 32)
             ^ static_cast<std::uint64_t>(static_cast<int>(p.second));
         return std::hash<std::uint64_t>()(key);
@@ -18,24 +17,16 @@ struct IntPairHash {
 class ChunkManager {
 public:
     ChunkManager();
-
     ~ChunkManager();
 
-
     void updateChunks(const sf::Vector2f& playerPos, const sf::View& view);
-    
     void draw(sf::RenderTarget& target, const sf::View& view);
-
-    
     void updateObjects(float dt);
 
 private:
     Chunk* createChunk(int cx, int cy);
-
-    int maxChunksX; 
-     int maxChunksY;
-
+    int maxChunksX;
+    int maxChunksY;
     std::unordered_map<std::pair<int, int>, Chunk*, IntPairHash> chunks;
-
     static constexpr int LOAD_RADIUS = 1;
 };
