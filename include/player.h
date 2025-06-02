d2d9a927dbbe02de3c4e14cf684fbb504176d65b
The4ing <atar.orram101@gmail.com>
2025-06-02 23:37:51 +0300

    Update player.h

--- include/player.h
+++ include/player.h
@@ -5,15 +5,18 @@
 class Player : public Character {
 public:
     Player();
+
     void setPosition(const sf::Vector2f& pos);
-
     sf::Vector2f getPosition() const;
-
     void update(float dt, const std::vector<sf::FloatRect>& blockedAreas);
-
     void draw(sf::RenderWindow& window);
+    sf::FloatRect getCollisionBounds(const sf::Vector2f& offset = {}) const;
+    sf::Vector2f getCenter() const;
+    float getCollisionRadius() const;
+
 
 private:
+
     sf::Sprite  sprite;
     float       speed = 50.f;
     int         frameWidth;
