cd8c512a7091f1467a684959d50eb1f11ea7a1a2
Romi Sinizkey <romisi@std.hac.ac.il>
2025-06-27 01:15:39 +0300

    Minigan change to Minigun

---
+++ src/Minigun.cpp
@@ -0,0 +1,10 @@
+#include "Minigun.h"
+#include "Player.h"
+
+Minigun::Minigun(const sf::Texture& texture, const sf::Vector2f& pos)
+    : WeaponPresent(texture, pos) {
+}
+
+std::string Minigun::getType() const {
+    return "Minigun";
+}
