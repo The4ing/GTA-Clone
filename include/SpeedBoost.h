897df3332e2abdd5c18f38d1ab312ed3bce1b8fe
Romi sinizkey <sinizromi@gmail.com>
2025-07-10 19:55:30 +0300

    Update SpeedBoost.h

--- include/SpeedBoost.h
+++ include/SpeedBoost.h
@@ -6,6 +6,5 @@ public:
     SpeedBoost(const sf::Texture& texture, const sf::Vector2f& pos);
     std::string getType() const override;
     void applyEffect(class Player& player) override;
-    int getPrice() const override;
 
 };
