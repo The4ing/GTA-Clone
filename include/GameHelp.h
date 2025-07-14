e0fbed8a381675d58a579436d3e7f78dae8e7a91
Romi sinizkey <sinizromi@gmail.com>
2025-07-15 02:23:30 +0300

    Create GameHelp.h

---
+++ include/GameHelp.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+class GameHelp {
+public:
+    GameHelp(sf::RenderWindow& window);
+    void run();
+
+private:
+    sf::RenderWindow& window;
+    sf::Font font;
+    std::vector<sf::Text> lines;
+
+    void setupText();
+};
