d3f89a0fb3ecdfc1189a537c48d01e4ad60b981a
Romi sinizkey <sinizromi@gmail.com>
2025-07-15 18:57:52 +0300

    Update Menu.h

--- include/Menu.h
+++ include/Menu.h
@@ -1,26 +1,26 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <vector>
 #include <string>
 
 class Menu {
 public:
-    Menu(sf::RenderWindow& window);
-
+    Menu(sf::RenderWindow& win);
     void update(sf::Event& event);
     void draw();
     int getSelectedIndex() const;
     bool isOptionChosen() const;
     std::string getSelectedOption() const;
-    void reset(); // Added reset method
+    void reset();
 
 private:
-
-
     sf::RenderWindow& window;
     sf::Font font;
+    sf::Sprite background;
     std::vector<sf::Text> options;
     int selectedIndex = 0;
     bool optionChosen = false;
-    sf::Sprite background;
+
+    sf::Sound menuLoop; // ✅ ניגון מוזיקת רקע
 };
